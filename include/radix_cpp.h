d31ecdf8a9797d0e0132b4da6df9aa5c569e6397
Mikael Rekola <mikael.rekola@gmail.com>
2024-01-28 21:41:54 +0200

    update implementation

--- include/radix_cpp.h
+++ include/radix_cpp.h
@@ -63,30 +63,41 @@ namespace radix_cpp {
 
       Iterator() { }
 
-      void addNode(Node * node) {
-	nodes_.push_back(node);
+      void addNode(SubTable * subtable, size_t idx) {
+	subtables_.push_back(subtable);
+	indices_.push_back(idx);
       }
 
-      reference operator*() const { return nodes_.back()->data; }
-      pointer operator->() { return &(nodes_.back()->data); }
+      reference operator*() const {
+	auto & subtable = subtables_.back();
+	size_t idx = indices_.back();
+	return (*subtable)[idx].data;
+      }
+      pointer operator->() {
+	auto & subtable = subtables_.back();
+	size_t idx = indices_.back();
+	return &((*subtable)[idx].data);
+      }
       Iterator& operator++() {
-	while ( !nodes_.empty() ) {
-	  size_t prefix = nodes_.back()->prefix_key;
+	while ( !indices_.empty() ) {
+	  auto & subtable = subtables_.back();
+	  size_t idx = indices_.back();
+	  size_t prefix = (*subtable)[idx].prefix_key;
 	  while ( 1 ) {
-	    nodes_.back()++;
-	    auto & node = nodes_.back();
-	    if (node->is_assigned) { // remove this
+	    indices_.back()++;
+	    auto & node = (*subtable)[indices_.back()];
+	    if (node.is_assigned) { // remove this
 	      return *this;
-	    } else if (!node->is_assigned) { // and this
+	    } else if (!node.is_assigned) { // and this
 	      continue;
-	    } else if (!node->is_assigned || node->is_sentinel || node->prefix_key != prefix) {
+	    } else if (!node.is_assigned || node.is_sentinel || node.prefix_key != prefix) {
 	      break;
-	    } else if (node->prefix_key == prefix) {
+	    } else if (node.prefix_key == prefix) {
 	      // fill_nodes();
 	      return *this;
 	    }
 	  }
-	  nodes_.pop_back();
+	  indices_.pop_back();
 	}
 	return *this;
       }
@@ -98,26 +109,31 @@ namespace radix_cpp {
       friend bool operator== (const Iterator& a, const Iterator& b) {
 	size_t n = a.size();
 	if (n != b.size()) return false;
-	for (size_t i = 0; i < n; i++) if (a.nodes_[i] != b.nodes_[i]) return false;
+	for (size_t i = 0; i < n; i++) if (a.indices_[i] != b.indices_[i]) return false;
 	return true;
       };
       friend bool operator!= (const Iterator& a, const Iterator& b) {
 	size_t n = a.size();
 	if (n != b.size()) return true;
-	for (size_t i = 0; i < n; i++) if (a.nodes_[i] != b.nodes_[i]) return true;
+	for (size_t i = 0; i < n; i++) if (a.indices_[i] != b.indices_[i]) return true;
 	return false;
       };
 
       void fast_forward() {
-	while (!nodes_.front()->is_assigned) {
-	  nodes_.front()++;
+	SubTable * table = subtables_.back();
+	while ( 1 ) {
+	  size_t idx = indices_.back();
+	  auto node = (*table)[idx];
+	  if (node.is_assigned) break;
+	  indices_.back()++;
 	}
       }
 
     private:
-      size_t size() const { return nodes_.size(); }
+      size_t size() const { return indices_.size(); }
 
-      std::vector<Node *> nodes_;
+      std::vector<SubTable *> subtables_;
+      std::vector<size_t> indices_;
     };
     
     typedef Iterator iterator;
@@ -145,8 +161,8 @@ namespace radix_cpp {
 	  key = (key + std::hash<uint64_t>{}(prefix_key)) % data_[i].size();
 	}
 	
+	it.addNode(&(data_[i]), key);
 	auto & node = data_[i][key];
-	it.addNode(&node);
 	if (!node.is_assigned) {
 	  is_new = false;
 	  node.data = vt;
@@ -160,7 +176,7 @@ namespace radix_cpp {
     Iterator begin() {
       Iterator it;
       for (size_t i = 0; i < data_.size(); i++) {
-	it.addNode(&(data_[i][0]));
+	it.addNode(&(data_[i]), 0);
       }
       it.fast_forward();
       return it;
@@ -168,7 +184,7 @@ namespace radix_cpp {
     Iterator end() {
       Iterator it;
       for (size_t i = 0; i < data_.size(); i++) {
-	it.addNode(&(data_[i].back()));
+	it.addNode(&(data_[i]), data_[i].size() - 1);
       }
       return it;
     }
