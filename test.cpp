c0b254fe80d394a40bd06c3986056a02e4dc92d7
Mikael Rekola <mikael.rekola@gmail.com>
2024-01-28 16:11:51 +0200

    update test environment

--- test.cpp
+++ test.cpp
@@ -3,19 +3,26 @@
 #include <iostream>
 
 int main(void) {
-  radix_cpp::set<unsigned short> S;
+  radix_cpp::set<uint8_t> S;
   S.insert(10);
-  S.insert(1255);
+  S.insert(125);
   for (auto & v : S) {
-    std::cout << v << "\n";
+    std::cout << (int)v << "\n";
   }
 
-  radix_cpp::map<unsigned short, std::string> M;
+  radix_cpp::map<uint8_t, std::string> M;
   M.insert(std::make_pair(1, "Hello"));
   M.insert(std::make_pair(2, "World"));
 
   for (auto & [ key, value ] : M) {
-    std::cout << key << " = " << value << "\n";
+    std::cout << (int)key << " = " << value << "\n";
   }
+
+  radix_cpp::set<uint16_t> S2;
+  S2.insert(1000);
+  S2.insert(1001);
+  S2.insert(1);
+  S2.insert(30000);
+  
   return 0;
 }
