3e28ea5824c3444645c2f6da1e5d0366b13eee6c
Mikael Rekola <mikael.rekola@gmail.com>
2024-02-02 01:50:30 +0200

    update test

--- test.cpp
+++ test.cpp
@@ -4,6 +4,11 @@
 #include <set>
 
 int main(void) {
+  radix_cpp::set<uint8_t> S0;
+  for (auto & v : S0) {
+    std::cerr << (int)v << "\n";
+  }
+
   radix_cpp::set<uint8_t> S;
   S.insert(65);
   S.insert(125);
@@ -54,11 +59,27 @@ int main(void) {
   }
 
   radix_cpp::set<uint32_t> S4;
+  S4.insert(1000);
+  auto it2 = S4.find(1000);
+  std::cerr << "*it2 = " << *it2 << "\n";
   for (uint32_t i = 0; i < 10000000; i++) {
     S4.insert(i);
   }
-  std::cerr << "S4, size = " << S4.size() << "\n";
+  std::cerr << "*it = " << *it2 << "\n";
+  std::cerr << "collisions per insert: "
+	    << (static_cast<double>(S4.num_insert_collisions()) / static_cast<double>(S4.num_inserts()))
+	    << "\n";
   
+  size_t n = 0;
+  for (auto & v : S4) {
+    if (n != v) {
+      std::cerr << "error, n = " << n << ", v = " << v << "\n";
+      exit(1);
+    }
+    n++;
+  }
+  std::cerr << "S4, size = " << S4.size() << ", calc size = " << n << "\n";
+
   radix_cpp::set<std::string> S5;
   S5.insert("Hello World");
   S5.insert("c");
@@ -67,14 +88,22 @@ int main(void) {
   S5.insert("A");
   S5.insert("B");
   S5.insert("Z");
-#if 0
   S5.insert("ab");
   S5.insert("abc");
   S5.insert("abcd");
-#endif
   S5.insert("@");
+  S5.insert("");
+
+  std::cerr << "trying to find empty string\n";
+  auto it3 = S5.find("");
+  if (it3 == S5.end()) {
+    std::cerr << "zero length element not found\n";
+    exit(1);
+  }
+
+  std::cerr << "iterating string\n";
   for (auto & v : S5) {
-    std::cerr << v << "\n";
+    std::cerr << "string: " << v << "\n";
   }
   
   return 0;
