281df5d6283ef623d71d53ed718c9ea58bbc7b80
Mikael Rekola <mikael.rekola@gmail.com>
2024-02-02 04:24:25 +0200

    update benchmarks

--- benchmark/b.cpp
+++ benchmark/b.cpp
@@ -31,43 +31,60 @@ static double get_wall_time() {
 }
 
 int main() {
-  std::map<int, std::pair<double, double> > results;
+  std::map<int, std::tuple<double, double, double, double> > results;
 
-  int runs = 1;
+  int runs = 5;
   for (int run = 0; run < runs; run++) {
     std::cerr << "run " << run << "\n";
     for (int n = 1000000; n <= 10000000; n += 500000) {
       std::cerr << "  test " << n << "\n";
       auto v = make_test_data(n);
       
-      double t0 = get_wall_time();
+      double t_a0, t_a1, t_a2;
+      double t_b0, t_b1, t_b2;
       
       {
-	radix_cpp::set<uint32_t> S1;
+	std::set<uint32_t> S1;
+	t_a0 = get_wall_time();
 	for (auto & a : v) {
 	  S1.insert(a);
 	}
+	t_a1 = get_wall_time();
+	int n = 0;
+	for (auto & a : S1) {
+	  n++;
+	}
+	t_a2 = get_wall_time();
       }
       
-      double t1 = get_wall_time();
-      
       {
-	std::set<uint32_t> S2;
+	radix_cpp::set<uint32_t> S2;
+	t_b0 = get_wall_time();
 	for (auto & a : v) {
 	  S2.insert(a);
 	}
+	t_b1 = get_wall_time();
+	int n = 0;
+	for (auto & a : S2) {
+	  n++;
+	}
+	t_b2 = get_wall_time();
       }
-      
-      double t2 = get_wall_time();
-      
-      results[n] = std::pair(t1 - t0, t2 - t1);
+
+      auto & r = results[n];
+      std::get<0>(r) += t_a1 - t_a0;
+      std::get<1>(r) += t_a2 - t_a1;
+      std::get<2>(r) += t_b1 - t_b0;
+      std::get<3>(r) += t_b2 - t_b1;
     }
   }
 
   for (auto & [ n, d ] : results) {
-    double t1 = d.first / runs;
-    double t2 = d.second / runs;
-    std::cout << n << ";" << t1 << ";" << t2 << "\n";
+    double t0 = std::get<0>(d) / runs;
+    double t1 = std::get<1>(d) / runs;
+    double t2 = std::get<2>(d) / runs;
+    double t3 = std::get<3>(d) / runs;
+    std::cout << n << ";" << t0 << ";" << t1 << ";" << t2 << ";" << t3 << "\n";
   }
 
   return 0;
