62dabe1e4ac7005ec23ca1beba7203abd044e8a7
Mikael Rekola <mikael.rekola@gmail.com>
2024-02-06 03:13:37 +0200

    add catch2

---
+++ tests/test.cpp
@@ -0,0 +1,131 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include <catch2/catch.hpp>
+
+#include "radix_cpp.h"
+
+TEST_CASE( "simple integer sets can be created", "[int_set]" ) {
+  radix_cpp::set<uint16_t> S;
+  S.insert(0);
+  S.insert(1);
+  S.insert(2);
+  auto it = S.begin();
+  REQUIRE(*it++ == 0);
+  REQUIRE(*it++ == 1);
+  REQUIRE(*it++ == 2);
+  REQUIRE(it == S.end());
+
+  radix_cpp::set<uint32_t> S2;
+  S2.insert(1000000000);
+  S2.insert(100000000);
+  S2.insert(10000000);
+  S2.insert(1000000);
+  S2.insert(100000);
+  S2.insert(10000);
+  S2.insert(1000);
+  S2.insert(100);
+  S2.insert(10);
+  S2.insert(1);
+  S2.insert(0);
+
+  auto it2 = S2.begin();
+  REQUIRE(*it2++ == 0);
+  REQUIRE(*it2++ == 1);
+  REQUIRE(*it2++ == 10);
+  REQUIRE(*it2++ == 100);
+  REQUIRE(*it2++ == 1000);
+  REQUIRE(*it2++ == 10000);
+  REQUIRE(*it2++ == 100000);
+  REQUIRE(*it2++ == 1000000);
+  REQUIRE(*it2++ == 10000000);
+  REQUIRE(*it2++ == 100000000);
+  REQUIRE(*it2++ == 1000000000);
+  REQUIRE(it2 == S2.end());
+}
+
+TEST_CASE( "large integer sets work and iterators are stable", "[large_set]") {
+  radix_cpp::set<uint32_t> S;
+  S.insert(1000);
+  auto it0 = S.find(1000);
+  
+  for (uint32_t i = 0; i < 1000000; i++) {
+    S.insert(i);
+  }
+  REQUIRE(*it0 == 1000);
+  
+#if 0
+  std::cerr << "collisions per insert: "
+	    << (static_cast<double>(S.num_insert_collisions()) / static_cast<double>(S.num_inserts()))
+	    << "\n";
+#endif
+  
+  uint32_t next_consecutive_element = 0;
+  for (auto & v : S) {
+    if (v != next_consecutive_element) {
+      break;
+    }
+    next_consecutive_element = v + 1;
+  }
+
+  REQUIRE(next_consecutive_element == 1000000);
+}
+
+TEST_CASE( "simple string sets can be created", "[string_set]" ) {
+  radix_cpp::set<std::string> S;
+  S.insert("a");
+  S.insert("bc");
+  S.insert("def");
+  auto it = S.begin();
+  REQUIRE(*it++ == "a");
+  REQUIRE(*it++ == "bc");
+  REQUIRE(*it++ == "def");
+  REQUIRE(it == S.end());
+}
+
+TEST_CASE( "empty string can be added to set", "[empty_string_set]" ) {
+  radix_cpp::set<std::string> S;
+  S.insert("abc");
+  S.insert("");
+  S.insert("gamma");
+  auto it = S.begin();
+  REQUIRE(*it++ == "");
+  REQUIRE(*it++ == "abc");
+  REQUIRE(*it++ == "gamma");
+  REQUIRE(it == S.end());
+}
+
+TEST_CASE( "shared prefixes work with strings", "[string_set_shared_prefix" ) {
+  radix_cpp::set<std::string> S;
+  S.insert("@");
+  S.insert("a");
+  S.insert("ab");
+  S.insert("abc");
+  S.insert("abcd");
+  S.insert("abcde");
+  S.insert("abcdef");
+  auto it = S.begin();
+  REQUIRE(*it++ == "@");
+  REQUIRE(*it++ == "a");
+  REQUIRE(*it++ == "ab");
+  REQUIRE(*it++ == "abc");
+  REQUIRE(*it++ == "abcd");
+  REQUIRE(*it++ == "abcde");
+  REQUIRE(*it++ == "abcdef");
+  REQUIRE(it == S.end());
+}
+
+TEST_CASE( "positive floating point keys", "[pos_float_set]") {
+  radix_cpp::set<float> S;
+  S.insert(1.1f);
+  S.insert(1000.0f);
+  S.insert(4.2f);
+  S.insert(30000000.f);
+  S.insert(0);
+  auto it = S.begin();
+  REQUIRE(*it++ == 0.0f);
+  REQUIRE(*it++ == 1.1f);
+  REQUIRE(*it++ == 4.2f);
+  REQUIRE(*it++ == 1000.0f);
+  REQUIRE(*it++ == 30000000.0f);
+  REQUIRE(it == S.end());
+}
